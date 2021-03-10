80f2d05585c5b5dd23552ec67ce31d5fdbddf3a6
erdemce <erdemce2234@gmail.com>
2021-03-10 17:13:02 +0100

    deployement try 3

--- app.js
+++ app.js
@@ -33,17 +33,16 @@ require('./config')(app);
 const path=require("path");
 app.use(express.static(path.join(__dirname,"public")));
 
-app.use((req, res, next) => {
-	// If no routes match, send them the React HTML.
-	res.sendFile(__dirname + "/public/index.html");
-});
-
-
 
 // 👇 Start handling routes here
 // Contrary to the views version, all routes are controled from the routes/index.js
 const allRoutes = require('./routes/index');
 app.use('/api', allRoutes);
+
+app.use((req, res, next) => {
+	// If no routes match, send them the React HTML.
+	res.sendFile(__dirname + "/public/index.html");
+});
 
 
 // ❗ To handle errors. Routes that don't exist or errors that you handle in specific routes
