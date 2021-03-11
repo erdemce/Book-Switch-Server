e0814eb5b070c745030eb601b4ca634d79529c60
erdemce <erdemce2234@gmail.com>
2021-03-11 10:08:17 +0100

    deployement with manish

--- config/index.js
+++ config/index.js
@@ -23,7 +23,7 @@ module.exports = (app) => {
   app.use(
     cors({
       credentials: true,
-      origin: process.env.ORIGIN || "https://book-switch.herokuapp.com",
+      origin: process.env.ORIGIN || "http://localhost:3000",
     })
   );
   const session = require('express-session');
